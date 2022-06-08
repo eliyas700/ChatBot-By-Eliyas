c405b0df5e3ddc54c5f8d0671c96d1665bd665c6
Eliyas Hossain <eliyashossain700@gmail.com>
2022-06-08 22:42:25 +0600

    Chat Bot Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Chatbot from "react-chatbot-kit";
+import Configuration from "./Chatbot/Configuration";
+import ActionProvider from "./Chatbot/ActionProvider";
+import MessageParser from "./Chatbot/MessageParser";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+        <div style={{ maxWidth: "300px" }}>
+          <Chatbot
+            config={Configuration}
+            actionProvider={ActionProvider}
+            messageParser={MessageParser}
+          />
+        </div>
+      </div>
     </div>
   );
 }
